4a2246402fc38988961fe39f1a13cead5a725bc9
jgsbaker <140206109+jgsbaker@users.noreply.github.com>
2023-11-15 15:42:03 -0500

    commit

--- fightBoredom/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function About(){
-    return (
-        <>
-            <h1>Fight Boredom</h1>
-            <p>Play a silly easy game to pass the time by.</p>
-        </>
-    )
-}
--- fightBoredom/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Home(){
-    return (
-        <>
-            <h1>Fight Boredom</h1>
-            <p>Click a link to explore some fun passive things to do when you're bored</p>
-        </>
-    )
-}
